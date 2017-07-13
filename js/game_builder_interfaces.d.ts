402f0075410a36d247d0727b3aa3bc617cc3faf4
Spiro Floropoulos <spirodonfl@gmail.com>
2017-07-12 18:46:13 -0700

    Forgot tile builder interface

--- js/game_builder_interfaces.d.ts
+++ js/game_builder_interfaces.d.ts
@@ -80,3 +80,27 @@ interface ICharacterBuilder {
     initialize(): void;
     generateCharacter(): void;
 }
+interface ITileBuilder {
+    // Reference to the window that you can hide or show and where the form is to save the tile
+    mainWindow: HTMLElement;
+    // Instead of checking the "display" style attribute, we just keep a boolean to see if the window is showing or not
+    mainWindowHidden: boolean;
+    // The canvas that houses the tileset image
+    canvasTileset: HTMLCanvasElement;
+    // The context for the tileset canvas
+    contextTileset: CanvasRenderingContext2D;
+    // The canvas that houses the 32x32 selection that you make when you click somewhere on the big tileset canvas
+    canvasTmp: HTMLCanvasElement;
+    // The context for the canvas that houses the selected tile
+    contextTmp: CanvasRenderingContext2D;
+    // The input element that contains the name of the tile
+    nameOfTile: HTMLInputElement;
+    // The actual image element that is in the window to show you what you've selected as you save
+    selectedImage: HTMLImageElement;
+
+    initialize(): void;
+    // Loads the tileset image and then updates the width / height of appropriate elements to match the image
+    setupCanvas(): void;
+    // Listens to thinks like the hover mouse trap, save button, etc...
+    setupListeners(): void;
+}
